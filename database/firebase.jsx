5c992e3b41071f1907551d5a46256897919e34ee
Daniel <danielarturo.marquez@gmail.com>
2022-03-28 09:24:51 -0400

    Update firebase.jsx

--- database/firebase.jsx
+++ database/firebase.jsx
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmq9TWYQ7rsgoTPpdhPU0-vz4v06FaV5s",
-  authDomain: "react-native-app-firebas-9c204.firebaseapp.com",
-  projectId: "react-native-app-firebas-9c204",
-  storageBucket: "react-native-app-firebas-9c204.appspot.com",
-  messagingSenderId: "637114156521",
-  appId: "1:637114156521:web:be5ac1d36c473b353a7778"
+  apiKey: "YOUR API KEY HERE",
+  authDomain: "YOUR AUTH DOMAIN HERE",
+  projectId: "YOUR PROJECT ID HERE",
+  storageBucket: "YOUR STORAGE BUCKET HERE",
+  messagingSenderId: "YOUR MSG SENDER ID HERE",
+  appId: "YOUR APP ID HERE"
 };
 
 // Initialize Firebase
